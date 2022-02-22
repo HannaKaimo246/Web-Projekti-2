301b12fab8c80e754db1c607b65fc5528ebdf490
HannaKaimo1975 <73232148+HannaKaimo1975@users.noreply.github.com>
2022-02-22 20:25:26 +0200

    Projekti ajan tasalle by Hanna

--- src/App.js
+++ src/App.js
@@ -7,118 +7,53 @@ import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
-import './styles/App.scss'
-import { Container } from "react-bootstrap"
-import {
-    BrowserRouter as Router,
-    Switch, Route, Link, useHistory
-} from 'react-router-dom'
-
-import HomePage from './views/HomePage'
-
-import {AuthProvider} from "./contexts/AuthContext"
-
-import PrivateRoute from "./components/User/PrivateRoute";
-
-import ForgotPassword from "./components/User/ForgotPassword"
-
-import Settings from "./components/User/Settings"
-
-//import PrivateChatPage from "./views/PrivateChatPage"
-
-import UserPage from "./views/UserPage"
-
-import LoginList from "./components/User/LoginList"
-
-//import {auth} from "./firebase"
 
 const App = () => {
-
-
-
-  // const [user, logout] = useAuth()
   const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
   const [weatherData, setWeatherData] = useState([]);
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState({});
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState(0);
 
-  useEffect(()  => {
-  navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude} }) => {
-    setCoordinates({lat: latitude, lng: longitude});
-    })
-  },[]);
-
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
+          setCoordinates({lat: latitude, lng: longitude});
+        })
+  }, []);
 
   useEffect(() => {
 
-    getWeatherData(coordinates.lat, coordinates.lng)
+      const filteredPlaces = places.filter((place) => place.rating > rating)
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
+
+    getWeatherData(bounds.sw, bounds.ne)
     .then((data) =>
         setWeatherData(data));
 
-    getPlacesData(bounds.sw, bounds.ne)
-        .then((data) => {
-          console.log(data);
-          setPlaces(data);
-        })
-      }, [coordinates, bounds]);
+    getPlacesData(type, bounds.sw, bounds.ne)
+    .then((data) => {
+      setFilteredPlaces([]);
+      setRating(0);
+    })
+  }, [type, coordinates,bounds]);
 
 
-    // const [user, logout] = useAuth()
-
-    const [user, logout] = useState('')
-
-    const history = useHistory()
-
-    const handleLogout = async () => {
-        // await auth.signOut()
-        // setUser(false)
-        try {
-            await logout()
-            history.pushState("api/login")
-        } catch {
-            alert("Uloskirjautuminen epäonnistui!")
-        }
-
-    }
-
-    // The back-to-top button is hidden at the beginning
-    const [showButton, setShowButton] = useState(false);
-
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.pageYOffset > 300) {
-                setShowButton(true);
-            } else {
-                setShowButton(false);
-            }
-        });
-    }, []);
-
-    // This function will scroll the window to the top
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth' // for smoothly scrolling
-        });
-    };
-
-    useEffect(() => {
-        const loggedInUser = localStorage.getItem("token");
-        if (loggedInUser) {
-
-            // setUser(true);
-        } else {
-            //   setUser(false);
-        }
-    }, []);
-
-    return (
-<>{/*
+return (
+<>
    <CssBaseline />
-   <Header />
+   <Header setCoordinates={setCoordinates} />
    <Grid container spacing={3} style={{ width: '100%'}}>
        <Grid item xs={12} md={4}>
-     <List places={places}
+     <List places={filteredPlaces.length ? filteredPlaces : places}
+           type={type}
+           setType={setType}
+           rating={rating}
+           setRating={setRating}
      />
        </Grid>
      <Grid item xs={12} md={8}>
@@ -126,70 +61,11 @@ const App = () => {
              setCoordinates={setCoordinates}
              setBounds={setBounds}
              coordinates={coordinates}
-             places={places}
+             places={filteredPlaces.length ? filteredPlaces : places}
              weatherData={weatherData}
              />
      </Grid>
    </Grid>
-    */}
-    <div id="app">
-        <Router>
-            <div id="sliderbar">
-                <nav>
-                    <ul>
-                        <figure className="image-logo">
-                            <img src={require("./assets/logo.png")} height="40px" width="40px" />
-                        </figure>
-                        <li>
-                            <Link className="navstyles" to="/">Etusivu<i className="ion-ios-home" /></Link>
-                        </li>
-                        {user &&
-                            <li>
-                                <Link className="navstyles" to="/api/privatechat">Private Chat<i
-                                    className="ion-md-chatbubbles"/></Link>
-                            </li>
-                        }
-                        <li>
-                            <Link className="navstyles" to="/api/map">Kartta<i className="ion-ios-map" /></Link>
-                        </li>
-                        {user &&
-                            <li>
-                                <Link className="navstyles" to="/api/settings">Asetukset<i className="ion-ios-settings" /></Link>
-                            </li>
-                        }
-
-                        {user
-                            ?   <div className="user">
-                                <p>Kirjautunut:</p>
-                                <Link to="/"><button type="button" onClick={handleLogout}>Kirjaudu-ulos</button></Link>
-                            </div>
-                            :    <div className="user">
-                                <Link to="/api/user/login"><button type="button">Kirjaudu sisään</button></Link>
-                                <Link to="/api/user/register"><button type="button">Rekisteröidy</button></Link>
-                            </div>
-                        }
-                    </ul>
-                </nav>
-            </div>
-            <AuthProvider>
-                <Switch>
-                    <PrivateRoute exact path="/api/settings" component={Settings} />
-                    { /* <Route path="/api/privatechat" component={PrivateChatPage} /> */}
-                    <Route path="/api/user/:id" component={UserPage} />
-                    <Route path="/api/forgot-password" component={ForgotPassword} />
-                    <Route path="/api/loginList" component={LoginList} />
-                    <Route path="/" component={HomePage} />
-                </Switch>
-            </AuthProvider>
-        </Router>
-
-        {showButton && (
-            <button onClick={scrollToTop} className="back-to-top">
-                &#8679;
-            </button>
-        )}
-
-    </div>
 </>
 );
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,50 +1,86 @@
-import React from 'react';
-import {Autocomplete} from '@react-google-maps/api';
+import React, {useState} from 'react';
 import {
   AppBar,
   Toolbar,
   Typography,
-  InputBase,
-  Box,
-  Grid,
+  Box
 } from '@material-ui/core';
 
 import useStyles from './styles';
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
-
+import { Autocomplete } from '@react-google-maps/api';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
 const AddLocation = () => {
 
-  console.log('TESTING');
+let ownUser = document.getElementById('nimimerkki').value;
+
+console.log(ownUser);
+
+let Marker = {
+
+  color: "red",
+  text: ownUser,
+  latitude: 60.192059,
+  longitude: 24.945831
 
 }
 
-const Header = () => {
+Map.add(Marker);
+}
+
+const Header = ({setCoordinates}) => {
   const classes = useStyles();
+  const [AutoComplete, setAutoComplete] = useState(null);
+
+  const onLoad = (AutoC) => setAutoComplete(AutoC);
+
+const onPlaceChanged = () => {
+
+  const lat = AutoComplete.getPlace().geometry.location.lat();
+  const lng = AutoComplete.getPlace().geometry.location.lng();
+
+  setCoordinates({lat, lng});
+
+}
+
 
   return (
 <AppBar position="static">
   <Toolbar className={classes.toolbar}>
     <Typography variant="h4" className={classes.title}>
-          Chat people locations
+          Chat Kartta
     </Typography>
     <Box display="flex">
       <Typography variant="h5" className={classes.title}>
-          Add new location
+          Lisää uusi chattaaja
       </Typography>
       <Input type="text" className={classes.title} placeholder="nimimerkki" id="nimimerkki">
       </Input>
-      <Button class="btn btn-primary" type="submit" onClick={AddLocation}>
-        Add
+      <Typography variant="h5" className={classes.title}>
+        Valitse markerisi väri
+      </Typography>
+      <Input type="color" id="markerinVari" className={classes.title} name="colorPicker"
+      placeholder="Valitse markerisi väri">
+      </Input>
+      <Button class="btn btn-primary" type="submit" id="locationButton" onClick={AddLocation}>
+        Lisää
       </Button>
       <Typography variant="h5" className={classes.title}>
-        Search
+        Etsi
       </Typography>
-      <Input type="text" className={classes.title} placeholder="nimimerkki" id="nimimerkki2">
-      </Input>
-      <Button class="btn btn-primary" type="submit" onClick={AddLocation}>
-        Search
+      <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+        <div className={classes.search}>
+<div className={classes.searchIcon}>
+<SearchIcon />
+</div>
+          <InputBase placeholder="Search..." classes={{root: classes.search}} />
+        </div>
+      </Autocomplete>
+      <Button class="btn btn-primary" type="submit">
+        Etsi
       </Button>
     </Box>
   </Toolbar>
